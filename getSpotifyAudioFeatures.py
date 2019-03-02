6d2de89016dd334a0da7c14dd2151579aab868b8
Robert Tuck <robtuck@gmail.com>
2019-03-02 18:40:30 -0500

    get audio features from Spotify

---
+++ getSpotifyAudioFeatures.py
@@ -0,0 +1,35 @@
+import csv
+
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+
+client_credentials_manager = SpotifyClientCredentials(client_id="ff5998c858ec4e148874d174d4ece89d", client_secret="33f1bdf6e3aa4397bc655601c92582bf")
+
+sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
+
+fh = open('test_spotify_audio_features.txt', 'w+')
+fhe = open('test_spotify_audio_features_error.txt', 'w+')
+
+columns = ["Track","Artist","DeezerId","isrc","SpotifyId"]
+
+# print("Track,Artist,DeezerId,isrc,SpotifyId", file=fh)
+headers = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature']
+header = ",".join(columns) + "," + ",".join(headers)
+
+print(header,fh)
+with open('test_spotifyId.txt') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=',')
+    for row in csv_reader:
+        res = sp.audio_features([row["SpotifyId"]])[0]
+        data = []
+        for h in headers:
+            data.append(str(res[h]))
+            # print(h + " " + str(res[h]))
+        row_data = ",".join([v for k, v in row.items()])
+        # print(row_data)
+        row_entry = ",".join(data)
+        # print(row_entry)
+        new_row = row_data + "," + row_entry
+        print(new_row,file=fh)
+
+fh.close()
