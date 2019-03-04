eafc0a53f2482d29de5194016258a5b29f397d3e
Robert Tuck <robtuck@gmail.com>
2019-03-03 21:43:24 -0500

    imporoved getting lyrics - parens

--- getGeniusLyrics.py
+++ getGeniusLyrics.py
@@ -1,10 +1,11 @@
 import csv
 import lyricsgenius
+import re
 
 fh = open('test_genius_lyrics.txt', 'w+')
 fhe = open('test_genius_lyrics_error.txt', 'w+')
 
-columns = ["Track","Artist","DeezerId","isrc","SpotifyId","GenuisURL","year","lyric"]
+columns = ["Track","Artist","DeezerId","isrc","SpotifyId","GenuisURL","lyric"]
 
 # print("Track,Artist,DeezerId,isrc,SpotifyId", file=fh)
 # headers = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature']
@@ -26,19 +27,21 @@ print(error_header, file=fhe)
 with open('test_spotifyId.txt') as csv_file:
     csv_reader = csv.DictReader(csv_file, delimiter=',')
     for row in csv_reader:
-        song = genius.search_song(row["Track"], row["Artist"])
         track = row["Track"]
+        search_track = re.sub(r"\s\(.*\)", "", track)
+        song = genius.search_song(search_track, row["Artist"])
         data = ",".join([v for k, v in row.items()])
         print(track)
         if song is None:
             print(data + ",NOT_FOUND", file=fhe)
-        elif song.title != row["Track"]:
+        elif song.title.upper() != search_track.upper():
             # print(row, file=fhe)
+            # print(song.title + " Track:" + search_track)
             print(data + ",ERROR", file=fhe)
         else:
             url = song.url
-            year = song.year
+            # print(url)
             lyric = song.lyrics
             plyric = lyric.replace("\n", " ")
-            print(data + "," + url + "," + year + "," + "\"" + plyric + "\"",file=fh)
+            print(data + "," + url + "," + "\"" + plyric + "\"",file=fh)
             # print(song.lyrics, file=fh)
