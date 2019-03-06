abb64deca9c45003be42d861de3c6b7cdc5c9c0a
Robert Tuck <robtuck@gmail.com>
2019-03-05 20:20:11 -0500

    added a loop for multiple files

--- getGeniusLyrics.py
+++ getGeniusLyrics.py
@@ -3,52 +3,60 @@ import lyricsgenius
 import re
 import time
 from random import randint
+import sys
 
 
-fh = open('test_genius_lyrics.txt', 'w+')
-fhe = open('test_genius_lyrics_error.txt', 'w+')
+files = sys.argv[1:]
+print(files)
 
-columns = ["Track","Artist","DeezerId","isrc","SpotifyId","GenuisURL","lyric"]
+for f in files:
+    fh = open('test_genius_lyrics_' + f + '.txt', 'w+')
+    fhe = open('test_genius_lyrics_' + f + '_error.txt', 'w+')
 
-# print("Track,Artist,DeezerId,isrc,SpotifyId", file=fh)
-# headers = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature']
-# header = ",".join(columns) + "," + ",".join(headers)
+    columns = ["Track","Artist","DeezerId","isrc","SpotifyId","GenuisURL","lyric"]
 
-ACCESS_TOKEN = "ONZMZdv2jGPJkK-f0yppCLUiwWhtUe3t6F6ufHc648TMoG2igt5TgBk7qhle6ovA"
+    # print("Track,Artist,DeezerId,isrc,SpotifyId", file=fh)
+    # headers = ['danceability', 'energy', 'key', 'loudness', 'mode', 'speechiness', 'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo', 'uri', 'track_href', 'analysis_url', 'duration_ms', 'time_signature']
+    # header = ",".join(columns) + "," + ",".join(headers)
 
-genius = lyricsgenius.Genius(ACCESS_TOKEN);
-genius.verbose = False
-genius.remove_section_headers = True
-genius.skip_non_songs = True
+    ACCESS_TOKEN = "ONZMZdv2jGPJkK-f0yppCLUiwWhtUe3t6F6ufHc648TMoG2igt5TgBk7qhle6ovA"
 
-header = ",".join(columns)
-error_header = ",".join(columns[0:5])
+    genius = lyricsgenius.Genius(ACCESS_TOKEN);
+    genius.verbose = False
+    genius.remove_section_headers = True
+    genius.skip_non_songs = True
 
-print(header, file=fh)
-print(error_header, file=fhe)
+    header = ",".join(columns)
+    error_header = ",".join(columns[0:5])
 
-with open('test_spotifyId.txt') as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=',')
-    for row in csv_reader:
-        track = row["Track"]
-        search_track = re.sub(r"\s\(.*\)", "", track)
-        sleep(randint(1, 3))
-        song = genius.search_song(search_track, row["Artist"])
-        data = ",".join([v for k, v in row.items()])
-        print(track)
-        if song is None:
-            print(data + ",NOT_FOUND", file=fhe)
-        elif song.title.upper() != search_track.upper():
-            # print(row, file=fhe)
-            # print(song.title + " Track:" + search_track)
-            print(data + ",ERROR", file=fhe)
-        else:
-            url = song.url
-            # print(url)
-            lyric = song.lyrics
-            plyric = lyric.replace("\n", " ")
-            print(data + "," + url + "," + "\"" + plyric + "\"",file=fh)
-            # print(song.lyrics, file=fh)
+    print(header, file=fh)
+    print(error_header, file=fhe)
 
-fh.close()
-fhe.close()
+    with open(f) as csv_file:
+        csv_reader = csv.DictReader(csv_file, delimiter=',', fieldnames=columns[0:5])
+
+        # next(csv_reader)
+
+        for row in csv_reader:
+            track = row["Track"]
+            search_track = re.sub(r"\s\(.*\)", "", track)
+            time.sleep(1)
+            song = genius.search_song(search_track, row["Artist"])
+            data = ",".join([v for k, v in row.items()])
+            print(track)
+            if song is None:
+                print(data + ",NOT_FOUND", file=fhe)
+            elif song.title.upper() != search_track.upper():
+                # print(row, file=fhe)
+                # print(song.title + " Track:" + search_track)
+                print(data + ",ERROR", file=fhe)
+            else:
+                url = song.url
+                # print(url)
+                lyric = song.lyrics
+                plyric = lyric.replace("\n", " ")
+                print(data + "," + url + "," + "\"" + plyric + "\"",file=fh)
+                # print(song.lyrics, file=fh)
+
+    fh.close()
+    fhe.close()
