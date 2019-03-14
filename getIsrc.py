17d1776f3bb7239a7cbefa57005082d631cc2f08
Robert Tuck <robtuck@gmail.com>
2019-03-14 01:34:06 -0400

    fixed dict references for csv_reader cleaned up code pep8 stuff

--- getIsrc.py
+++ getIsrc.py
@@ -3,20 +3,11 @@ import requests
 import json
 import time
 
-from json.decoder import JSONDecodeError
-
 headers = {
-    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3)" +
+                  " AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"}
 
 token = "frMuTEC5FiFQ9uSImyjXhr7CL2MNvPL65y9hWWQ95OjhL1AnGj"
-
-
-def get_isrc(track_id):
-    query = "https://api.deezer.com/track/" + str(track_id) + "&access_key=" + token
-    time.sleep(0.125)
-    results = requests.get(query, headers=headers).content
-
-    return results.decode('utf-8')
 
 
 fh = open('test_isrc.txt', 'w+')
@@ -31,10 +22,9 @@ with open('test_deezerId.txt') as csv_file:
     for row in csv_reader:
         track_id = row["deezerId"]
         unique_id = row["uniqueSongId"]
-        track = row["Track"]
-        artist = row["Artist"]
+        track = row["title"]
+        artist = row["artist"]
         query = "https://api.deezer.com/track/" + str(track_id)
-
 
         time.sleep(0.125)
         try:
@@ -48,7 +38,8 @@ with open('test_deezerId.txt') as csv_file:
                 deezer_title = test["title"]
                 print(track + "\t" + artist + "\t" + str(unique_id) + "\t" + str(track_id) + "\t" + isrc, file=fh)
             else:
-                print(track + "\t" + artist + "\t" + str(unique_id) + "\t" + str(track_id) + "\t" + "NOT_FOUND", file=fhe)
+                print(track + "\t" + artist + "\t" + str(unique_id) + "\t" + str(track_id) + "\t" + "NOT_FOUND",
+                      file=fhe)
         except KeyError:
             print(track + "\t" + artist + "\t" + str(unique_id) + "\t" + str(track_id) + "\t" + "ERROR", file=fhe)
             continue
