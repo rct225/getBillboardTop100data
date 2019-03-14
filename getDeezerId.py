92871e49c1f0ee1dda08b32ee726368f1452d816
Robert Tuck <robtuck@gmail.com>
2019-03-14 00:22:52 -0400

    let's try it in a try block

--- getDeezerId.py
+++ getDeezerId.py
@@ -59,17 +59,21 @@ for song,value in unique_songs.items():
     endpoint = "https://api.deezer.com/search?q="
 
     time.sleep(0.125)
-    results = requests.get(endpoint + query, headers=headers).content
+    try:
+        results = requests.get(endpoint + query, headers=headers).content
 
-    data = results.decode('utf-8')
-    test = json.loads(data)
-    print(song + "," + artist)
-    res = test["data"]
-    if res:
-        p = res[0]
-        print(p["title"] + "\t" + p["artist"]["name"] + "\t" + str(uniqueSongId) + "\t" + str(p["id"]), file=fh)
-    else:
-        print(song + "\t" + artist + "\t" + str(uniqueSongId) + "\t" + "NOT_FOUND", file=fhe)
+        data = results.decode('utf-8')
+        test = json.loads(data)
+        print(song + "," + artist)
+        res = test["data"]
+        if res:
+            p = res[0]
+            print(p["title"] + "\t" + p["artist"]["name"] + "\t" + str(uniqueSongId) + "\t" + str(p["id"]), file=fh)
+        else:
+            print(song + "\t" + artist + "\t" + str(uniqueSongId) + "\t" + "NOT_FOUND", file=fhe)
+    except KeyError:
+        print(song + "\t" + artist + "\t" + str(uniqueSongId) + "\t" + "ERROR", file=fhe)
+        continue
 
 fhe.close()
 fh.close()
