50fd4fd50d19f21fc209bd60c7fd2f770df03887
Robert Tuck <robtuck@gmail.com>
2019-03-14 01:36:02 -0400

    fixed column names moved over to tsv cleaned up comments

--- getSpotifyId.py
+++ getSpotifyId.py
@@ -1,33 +1,36 @@
 import csv
-# import requests
-import pprint
-import re
-
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-import json
 
-client_credentials_manager = SpotifyClientCredentials(client_id="ff5998c858ec4e148874d174d4ece89d", client_secret="33f1bdf6e3aa4397bc655601c92582bf")
+
+client_credentials_manager = SpotifyClientCredentials(client_id="ff5998c858ec4e148874d174d4ece89d",
+                                                      client_secret="33f1bdf6e3aa4397bc655601c92582bf")
 
 sp = spotipy.Spotify(client_credentials_manager=client_credentials_manager)
 
 fh = open('test_spotifyId.txt', 'w+')
 fhe = open('test_spotifyId_error.txt', 'w+')
 
-# Track,Artist,DeezerId,isrc
-print("Track,Artist,DeezerId,isrc,SpotifyId", file=fh)
+
+print("title\tartist\tuniqueSongId\tdeezerId\tisrc\tspotifyId", file=fh)
+print("title\tartist\tuniqueSongId\tdeezerId\tisrc\tspotifyId", file=fhe)
 
 with open('test_isrc.txt') as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=',')
+    csv_reader = csv.DictReader(csv_file, delimiter='\t')
     for row in csv_reader:
         isrc = row["isrc"]
         print(isrc)
-        result = sp.search(q="isrc: "+isrc, limit=20, offset=0, type='track')
-        if len(result["tracks"]["items"]) > 0:
-            data = ",".join([v for k,v in row.items()])
-            print(data + "," + result["tracks"]["items"][0]["id"], file=fh)
-        else:
-            print("Can't find" + row["Track"], file=fhe)
+        data = "\t".join([v for k, v in row.items()])
+        try:
+            result = sp.search(q="isrc: "+isrc, limit=20, offset=0, type='track')
+            if len(result["tracks"]["items"]) > 0:
+
+                print(data + "\t" + result["tracks"]["items"][0]["id"], file=fh)
+            else:
+                print(data + "\t" + "NOT_FOUND", file=fhe)
+        except:
+            print(data + "\t" + "ERROR", file=fhe)
+            continue
 
 fh.close()
 fhe.close()
